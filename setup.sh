ca442c24d9770a81e3a8e57ada4ccb4cdb4d5644
Dominik <dominik@borchert.me>
2022-07-10 14:12:32 +0200

    First change

--- setup.sh
+++ setup.sh
@@ -1,1 +1,35 @@
+#!/usr/bin/env bash
 
+echo "Starting Setup Script"
+
+# Update Ubuntu
+apt-get update
+
+# Install git
+sudo apt install -y git
+
+# Configure firewall
+sudo ufw allow OpenSSH && \
+  sudo ufw allow http && \
+  sudo ufw allow https && \
+  sudo ufw enable
+
+# Install docker
+sudo apt-get update
+sudo apt-get install \
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release
+    
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+ 
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+echo "Setup Done! Finished installing Git and Docker and setting up the firewall."
