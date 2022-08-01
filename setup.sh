b96bff6f3c82a92bfe6108248644d5470716e018
Dominik Borchert <dominik.borchert@arvato-scs.com>
2022-08-01 18:20:53 +0200

    Added docker-compose

--- setup.sh
+++ setup.sh
@@ -21,15 +21,20 @@ sudo apt-get install \
   curl \
   gnupg \
   lsb-release
-    
+
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
- 
+
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
 
 echo "Setup Done! Finished installing Git and Docker and setting up the firewall."
